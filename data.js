9f59f2dbce162670729992a0a587cfb653f56936
Matheus <matheusfrotacsc@edu.unifor.br>
2025-12-18 21:05:03 -0300

    Revert "teste 2"
    This reverts commit 10ee3a3b6248d44ec04402fd23c31f77b1b432aa.

--- data.js
+++ data.js
@@ -1,6 +1,15 @@
 const dados = {
-  "romeu01": "Maya",
-  "maya02": "Gabriel",
-  "lara03": "Romeu",
-  "gabriel04": "Lara"
+  "vobeto01": "Marina",
+  "alberto02": "Priscila",
+  "arthur03": "Juliana",
+  "clara04": "Matheus",
+  "julia05": "Vô Beto",
+  "marina06": "Arthur",
+  "matheusf07": "Brenda",
+  "melissa08": "Nilce",
+  "matheus09": "Clara",
+  "juliana10": "Alberto Cid",
+  "nilce11": "Matheus Filho",
+  "priscila12": "Júlia",
+  "brenda13": "Melissa"
 };
